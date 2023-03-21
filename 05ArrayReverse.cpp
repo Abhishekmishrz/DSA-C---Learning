4240cb43437e1258afe210d03fd92dd2bd97384e
Abhishek <alm03060199@gmail.com>
2023-03-21 17:55:36 +0530

    update

---
+++ 05ArrayReverse.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// Driver Code
+int arrayReverse(int arr[],int n){
+    int s = 0;
+    int e = n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s += 1;
+        e -= 1;
+    }
+
+};
+int main()
+{
+    int arr[] = {1,2,5,8,25,10,16,19,22,26,29,35,40};
+    int n = sizeof(arr) / sizeof(int);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    arrayReverse(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ 06PrintingPairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+// Driver Code
+int PrintPair(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int x = arr[i];
+        for(int j=i+1;j<n;j++){
+            int y = arr[j];
+            cout<<"("<<arr[i] <<","<< y<<")" << endl;
+        }
+        cout<<endl;
+        
+    }
+
+
+}
+int main()
+{
+    int arr[] ={ 1,2,5,8,25,10,16,19};
+    int n = sizeof(arr)/sizeof(int);
+    PrintPair(arr, n);
+    // for(int x: arr){
+    //     cout<<x<<endl;
+    // }
+    
+    return 0;
+}
